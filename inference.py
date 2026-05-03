53dd1a3daf447e5057db9451ef2874d156d5d95d
chiragviper1997 <chiragbathla19@gmail.com>
2026-05-03 18:44:03 +0530

    files

---
+++ inference.py
@@ -0,0 +1,33 @@
+import json
+import argparse
+from src.rag_pipeline import rag_pipeline
+
+
+def main(input_path, output_path):
+    with open(input_path, "r") as f:
+        data = json.load(f)
+
+    results = []
+
+    for item in data:
+        query = item["query"]
+
+        standards, latency = rag_pipeline(query)
+
+        results.append({
+            "id": item["id"],
+            "retrieved_standards": standards,
+            "latency_seconds": latency
+        })
+
+    with open(output_path, "w") as f:
+        json.dump(results, f, indent=2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--output", required=True)
+
+    args = parser.parse_args()
+    main(args.input, args.output)
